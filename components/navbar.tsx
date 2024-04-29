4fd35068f7674a0b77b76f8301311a596cf7952e
ED <ednoosde.wob@gmail.com>
2024-04-29 01:28:56 -0400

    Initial commit

---
+++ components/navbar.tsx
@@ -0,0 +1,158 @@
+import {
+	Navbar as NextUINavbar,
+	NavbarContent,
+	NavbarMenu,
+	NavbarMenuToggle,
+	NavbarBrand,
+	NavbarItem,
+	NavbarMenuItem,
+} from "@nextui-org/navbar";
+import { Button } from "@nextui-org/button";
+import { Kbd } from "@nextui-org/kbd";
+import { Link } from "@nextui-org/link";
+import { Input } from "@nextui-org/input";
+
+import { link as linkStyles } from "@nextui-org/theme";
+
+import { siteConfig } from "@/config/site";
+import NextLink from "next/link";
+import clsx from "clsx";
+
+import { ThemeSwitch } from "@/components/theme-switch";
+import {
+	TwitterIcon,
+	GithubIcon,
+	DiscordIcon,
+	HeartFilledIcon,
+	SearchIcon,
+} from "@/components/icons";
+
+import { Logo } from "@/components/icons";
+
+export const Navbar = () => {
+	const searchInput = (
+		<div className="rounded-2xl flex justify-center items-center bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg">
+		<Input
+		  label="Search"
+		  isClearable
+		  radius="lg"
+		  classNames={{
+			label: "text-black/50 dark:text-white/90",
+			input: [
+			  "bg-transparent",
+			  "text-black/90 dark:text-white/90",
+			  "placeholder:text-default-700/50 dark:placeholder:text-white/60",
+			],
+			innerWrapper: "bg-transparent",
+			inputWrapper: [
+			  "shadow-xl",
+			  "bg-default-200/50",
+			  "dark:bg-default/60",
+			  "backdrop-blur-xl",
+			  "backdrop-saturate-200",
+			  "hover:bg-default-200/70",
+			  "dark:hover:bg-default/70",
+			  "group-data-[focused=true]:bg-default-200/50",
+			  "dark:group-data-[focused=true]:bg-default/60",
+			  "!cursor-text",
+			],
+		  }}
+		  placeholder="Type to search..."
+		  startContent={
+			<SearchIcon className="text-black/50 mb-0.5 dark:text-white/90 text-slate-400 pointer-events-none flex-shrink-0" />
+		  }
+		/>
+	  </div>
+	);
+
+	return (
+		<NextUINavbar maxWidth="xl" position="sticky">
+			<NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+				<NavbarBrand as="li" className="gap-3 max-w-fit">
+					<NextLink className="flex justify-start items-center gap-1" href="/">
+						<Logo />
+						<p className="font-bold text-inherit">ACME</p>
+					</NextLink>
+				</NavbarBrand>
+				<ul className="hidden lg:flex gap-4 justify-start ml-2">
+					{siteConfig.navItems.map((item) => (
+						<NavbarItem key={item.href}>
+							<NextLink
+								className={clsx(
+									linkStyles({ color: "foreground" }),
+									"data-[active=true]:text-primary data-[active=true]:font-medium"
+								)}
+								color="foreground"
+								href={item.href}
+							>
+								{item.label}
+							</NextLink>
+						</NavbarItem>
+					))}
+				</ul>
+			</NavbarContent>
+
+			<NavbarContent
+				className="hidden sm:flex basis-1/5 sm:basis-full"
+				justify="end"
+			>
+				<NavbarItem className="hidden sm:flex gap-2">
+					<Link isExternal href={siteConfig.links.twitter} aria-label="Twitter">
+						<TwitterIcon className="text-default-500" />
+					</Link>
+					<Link isExternal href={siteConfig.links.discord} aria-label="Discord">
+						<DiscordIcon className="text-default-500" />
+					</Link>
+					<Link isExternal href={siteConfig.links.github} aria-label="Github">
+						<GithubIcon className="text-default-500" />
+					</Link>
+					<ThemeSwitch />
+				</NavbarItem>
+				<NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
+				<NavbarItem className="hidden md:flex">
+					<Button
+            isExternal
+						as={Link}
+						className="text-sm font-normal text-default-600 bg-default-100"
+						href={siteConfig.links.sponsor}
+						startContent={<HeartFilledIcon className="text-danger" />}
+						variant="flat"
+					>
+						Sponsor
+					</Button>
+				</NavbarItem>
+			</NavbarContent>
+
+			<NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
+				<Link isExternal href={siteConfig.links.github} aria-label="Github">
+					<GithubIcon className="text-default-500" />
+				</Link>
+				<ThemeSwitch />
+				<NavbarMenuToggle />
+			</NavbarContent>
+
+			<NavbarMenu>
+				{searchInput}
+				<div className="mx-4 mt-2 flex flex-col gap-2">
+					{siteConfig.navMenuItems.map((item, index) => (
+						<NavbarMenuItem key={`${item}-${index}`}>
+							<Link
+								color={
+									index === 2
+										? "primary"
+										: index === siteConfig.navMenuItems.length - 1
+										? "danger"
+										: "foreground"
+								}
+								href="#"
+								size="lg"
+							>
+								{item.label}
+							</Link>
+						</NavbarMenuItem>
+					))}
+				</div>
+			</NavbarMenu>
+		</NextUINavbar>
+	);
+};
