bc55203561c546015f54eb16553fa708235c96e2
ED <ednoosde.wob@gmail.com>
2024-05-11 13:59:13 -0400

    Addded Forms for Login and Register

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -6,10 +6,12 @@ import React from "react";
 import {Button, Input, Checkbox, Link, Divider} from "@nextui-org/react";
 import {Icon} from "@iconify/react";
 
-export default function Component() {
+export default function RegisterForm() {
   const [isVisible, setIsVisible] = React.useState(false);
+  const [isConfirmVisible, setIsConfirmVisible] = React.useState(false);
 
   const toggleVisibility = () => setIsVisible(!isVisible);
+  const toggleConfirmVisibility = () => setIsConfirmVisible(!isConfirmVisible);
 
   const inputClasses: InputProps["classNames"] = {
     inputWrapper:
@@ -21,9 +23,19 @@ export default function Component() {
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-gradient-to-br from-rose-400 via-fuchsia-500 to-indigo-500 p-2 sm:p-4 lg:p-8">
       <div className="flex w-full max-w-sm flex-col gap-4 rounded-large bg-background/60 px-8 pb-10 pt-6 shadow-small backdrop-blur-md backdrop-saturate-150 dark:bg-default-100/50">
-        <p className="pb-2 text-xl font-medium">Log In</p>
+        <p className="pb-2 text-xl font-medium">Sign Up</p>
         <form className="flex flex-col gap-3" onSubmit={(e) => e.preventDefault()}>
           <Input
+            isRequired
+            classNames={inputClasses}
+            label="Username"
+            name="username"
+            placeholder="Enter your username"
+            type="text"
+            variant="bordered"
+          />
+          <Input
+            isRequired
             classNames={inputClasses}
             label="Email Address"
             name="email"
@@ -32,6 +44,7 @@ export default function Component() {
             variant="bordered"
           />
           <Input
+            isRequired
             classNames={inputClasses}
             endContent={
               <button type="button" onClick={toggleVisibility}>
@@ -54,22 +67,50 @@ export default function Component() {
             type={isVisible ? "text" : "password"}
             variant="bordered"
           />
-          <div className="flex items-center justify-between px-1 py-2">
-            <Checkbox
-              classNames={{
-                wrapper: "before:border-foreground/50",
-              }}
-              name="remember"
-              size="sm"
-            >
-              Remember me
-            </Checkbox>
-            <Link className="text-foreground/50" href="#" size="sm">
-              Forgot password?
+          <Input
+            isRequired
+            classNames={inputClasses}
+            endContent={
+              <button type="button" onClick={toggleConfirmVisibility}>
+                {isConfirmVisible ? (
+                  <Icon
+                    className="pointer-events-none text-2xl text-foreground/50"
+                    icon="solar:eye-closed-linear"
+                  />
+                ) : (
+                  <Icon
+                    className="pointer-events-none text-2xl text-foreground/50"
+                    icon="solar:eye-bold"
+                  />
+                )}
+              </button>
+            }
+            label="Confirm Password"
+            name="confirmPassword"
+            placeholder="Confirm your password"
+            type={isConfirmVisible ? "text" : "password"}
+            variant="bordered"
+          />
+          <Checkbox
+            isRequired
+            classNames={{
+              base: "py-4",
+              label: "text-foreground/50",
+              wrapper: "before:border-foreground/50",
+            }}
+            size="sm"
+          >
+            I agree with the&nbsp;
+            <Link color="foreground" href="#" size="sm">
+              Terms
             </Link>
-          </div>
+            &nbsp; and&nbsp;
+            <Link color="foreground" href="#" size="sm">
+              Privacy Policy
+            </Link>
+          </Checkbox>
           <Button className={buttonClasses} type="submit">
-            Log In
+            Sign Up
           </Button>
         </form>
         <div className="flex items-center gap-4 py-2">
@@ -86,9 +127,9 @@ export default function Component() {
           </Button>
         </div>
         <p className="text-center text-small text-foreground/50">
-          Need to create an account?&nbsp;
-          <Link color="foreground" href="/auth/register" size="sm">
-            Sign Up
+          Already have an account?&nbsp;
+          <Link color="foreground" href="/auth/login" size="sm">
+            Log In
           </Link>
         </p>
       </div>
